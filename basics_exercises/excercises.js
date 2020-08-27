94341252c849502ca9e56e2492f9d5841311a79d
Jourden Riley <jourdenrai@gmail.com>
2020-08-27 13:00:20 -0500

    Add basics exercises folder

---
+++ basics_exercises/excercises.js
@@ -0,0 +1,93 @@
+/* 1. Concatenate two or more strings, one with your first name and one with your last, 
+to create a string with your full name as its value. For example, if your name is John Doe, 
+think about how you can put 'John' and 'Doe' together to get 'John Doe'.*/
+
+// let firstName = "Jourden",
+//     lastName = "Riley",
+//     fullName;
+
+// fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// 2. Use the arithmetic operators to determine the individual digits of a 4-digit number like 4936
+
+// let num = 4936;
+
+// let ones = (num % 10);
+// console.log(ones);
+
+// num = (num - ones) / 10;
+// let tens = num % 10;
+// console.log(tens);
+
+// num = (num - tens) / 10;
+// let hundreds = num % 10;
+// console.log(hundreds);
+
+// num = (num - hundreds) / 10;
+// let thousands = num % 10;
+// console.log(thousands);
+
+// console.table()
+
+// 3. Identify the data type for each of the following values.
+
+// 'true' = string
+// false = boolean
+// 1.5 = float
+// 2 = number
+// undefined = undefined
+// {foo: 'bar} = object
+
+// 4. Given the code below, identify which lines are statements and which are expressions. 
+
+//let foo => statement
+
+//foo = 5 => expression
+
+//foo; => expression
+
+// 5. Explain why this code logs '510' instead of 15. console.log('5' + 10);
+
+// ANSWER HERE 
+
+// 6. Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead.
+
+// console.log(5 + 10);
+
+// 7. Use the template literal syntax along with the expression Number('5') + 10 to log the following sentence to the console:
+
+// console.log(`The value of 5 + 10 is ${Number('5') + 10}.`)
+
+// 8. Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array? For example:
+
+// let foo = ['a', 'b', 'c'];
+// foo.length;  // returns 3
+// foo[3];      // will this result in an error?
+
+//ANSWER HERE
+
+// 9. Create an array named names that contains a list of pet names.
+
+// let petNames = ["Oscar","Rico","Nala"];
+
+// 10. Create an object named pets that contains a list of pet names and the type of animal.
+
+// let pets = {
+//     oscar: "Dog", 
+//     rico: "Dog", 
+//     nala: "Dog"
+//   };
+
+// 11. What value does the following expression evaluate to?
+
+// 'foo' == 'Foo' => false
+
+// 12. What value does the following expression evaluate to?
+
+// parseInt('3.1415) => 3
+
+// 13. What value does the following expression evaluate to?
+
+// '12' < '9' => true
